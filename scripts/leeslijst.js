1053a87ee86daf7fb56f905cbe7f81e5f2be3056
DivaniNL <dylanvannierop@gmail.com>
2024-12-05 08:51:53 +0100

    added code added during workshop

---
+++ scripts/leeslijst.js
@@ -0,0 +1,19 @@
+// Als je op de leeslijst button klikt, krijgt deze een andere kleur.
+// Als dit item al in de leeslijst zit, maak de kleur dan weer normaal
+
+// Dit item wordt nu toegevoegd aan de array van de LocalStorage. 
+// (check of er al een localstorage item is, zo niet maak er dan een aan)
+// Deze bestaat uit een link, titel, en introtekst
+// Als dit item al in de local storage staat, verwijder hem dan
+
+// In de header wordt de count geupdated
+// Als er nog geen counter bolletje is, voeg de class toe aan dit menu item.
+//////////////////////
+
+// Als je op desktop op de leeslijst item in de navigatie klikt, wordt het standard event genegeerd (preventdefault)
+// Hierna wordt een class gegeven aan de leeslijst tooltip waardoor deze zichtbaar wordt.
+
+// ////////////////
+// Als je op de leeslijst pagina bent wordt de localstorage uitgelezen.
+// Als er nog niks in de leeslijst zit, laat dan een bericht zien wat dit duidelijk maakt.(empty state) 
+// Als er iets in de leeslijst zit, haal de leeslijst op met LocalStorage.GetItem en plaats voor elk item in de array een article element.
