a611dc0773fe01f8998f32f5e3f407702cea923f
DivaniNL <dylanvannierop@gmail.com>
2024-12-12 10:23:04 +0100

    #29 Added fallback for non-support

--- scripts/share.js
+++ scripts/share.js
@@ -19,15 +19,22 @@ console.log(sharePage);
 const shareBtn = document.querySelector(".shareBtn");
 const resultPara = document.querySelector(".result");
 
-
-shareBtn.addEventListener("click", async () => {
- try {
-   // Share
-   await navigator.share(sharePage);
-   // Succes melding
-   resultPara.textContent = "Arikel sucessvol gedeeld";
- } catch (err) {
-   // Oops i did it again...
-   resultPara.textContent = `Oeps! Er is iets fout gegaan: ${err}`;
- }
-});
+if (1 == 0) {
+ shareBtn.addEventListener("click", async () => {
+   try {
+     // Share
+     await navigator.share(sharePage);
+     // Succes melding
+     resultPara.textContent = "Artikel succesvol gedeeld";
+   } catch (err) {
+     // Oops, something went wrong
+     resultPara.textContent = `Oeps! Er is iets fout gegaan: ${err}`;
+   }
+ });
+} else {
+ // Als Share API niet werkt. Sadge.
+ shareBtn.addEventListener("click", () => {
+   resultPara.textContent = "Deel functie wordt niet ondersteund in deze browser.";
+   console.warn("De functie navigator.share wordt niet ondersteund in deze browser. Dat is jammer, ik wilde net wat cools doen 😞 Ik ga even huilen in een hoekje...");
+ });
+}
